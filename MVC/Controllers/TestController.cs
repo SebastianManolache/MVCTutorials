945120474ae7107141183ea6369c565a7ea620bf
sebastianManolache <sebastian.manolache@assist.ro>
2019-05-07 11:42:52 +0300

    MVC-Day1-Lab1-TestController

--- MVC/Controllers/TestController.cs
+++ MVC/Controllers/TestController.cs
@@ -6,14 +6,25 @@ using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
-    public class TestController : Controller
+    public class Customer
     {
-        public string GetString()
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
         {
-            return "Hello World is old now. It’s time for wassup bro ;)";
+            return this.CustomerName + "|" + this.Address;
         }
     }
-    
-    
+    public class TestController : Controller
+    {
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address1";
+            return c;
+        }
     }
+
+
 }
