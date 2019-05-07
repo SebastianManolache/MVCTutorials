d98d7e069d28f029c82594aa7efc3df88627b9ae
sebastianManolache <sebastian.manolache@assist.ro>
2019-05-07 11:10:59 +0300

    Setup

---
+++ MVC/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+    }
+    
+    
+    }
+}
