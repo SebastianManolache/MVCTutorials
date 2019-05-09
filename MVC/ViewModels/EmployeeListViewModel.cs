c4df731b0b10ab2265f9ad14b08c1ef56139c43e
sebastianManolache <sebastian.manolache@assist.ro>
2019-05-09 17:32:47 +0300

    MVC-Day2-Lab7-coding style update

--- MVC/ViewModels/EmployeeListViewModel.cs
+++ MVC/ViewModels/EmployeeListViewModel.cs
@@ -1,14 +1,9 @@
-﻿using MVC.Models;
-using MVC.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 
 namespace MVC.ViewModels
 {
-       
+
     public class EmployeeListViewModel
     {
        
