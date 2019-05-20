f70a4db98377ba75643d838fc3ca255d41eda04b
sebastianManolache <sebastian.manolache@assist.ro>
2019-05-20 12:05:04 +0300

    MVC-day6-Lab 30-Exception Handling -Log Exception

--- MVC/App_Start/FilterConfig.cs
+++ MVC/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using MVC.Filters;
+using System.Web.Mvc;
 
 namespace MVC
 {
@@ -7,6 +8,8 @@ namespace MVC
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new EmployeeExceptionFilter());
+            //filters.Add(new AuthorizeAttribute());
         }
     }
 }
