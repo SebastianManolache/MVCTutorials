329bb65bcc1aa189d5039d93276d27fcdf9a1c78
sebastianManolache <sebastian.manolache@assist.ro>
2019-05-21 10:33:37 +0300

    MVC-Day7-LAb33-Creating single page application-Part 1-Setup

---
+++ MVC/Areas/SPA/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.ViewModels.SPA;
+using OldViewModel = MVC.ViewModels;
+
+namespace MVC.Areas.SPA.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            var view = new MainViewModel();
+            view.UserName = User.Identity.Name;
+            view.FooterData = new OldViewModel.FooterViewModel();
+            view.FooterData.CompanyName = "Internship ASSIST";
+            view.FooterData.Year = DateTime.Now.Year.ToString();
+            return View("Index", view);
+        }
+    }
+}
